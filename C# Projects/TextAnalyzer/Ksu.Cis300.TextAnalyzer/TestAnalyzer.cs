6dbd39b3367587de22b8be395b0c33e33903aab8
Rylan Kasitz <rylankasitz@ksu.edu>
2018-02-06 09:39:39 -0600

    Added text analyzer

---
+++ C# Projects/TextAnalyzer/Ksu.Cis300.TextAnalyzer/TestAnalyzer.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ksu.Cis300.Sort;
+using System.Text.RegularExpressions;
+
+namespace Ksu.Cis300.TextAnalyzer
+{
+    static class TestAnalyzer
+    {
+        /// <summary>
+        /// Adds words to the dictionary and returns amount of words added
+        /// </summary>
+        /// <param name="fileName">File being added</param>
+        /// <param name="fileNums">The file number</param>
+        /// <param name="dic">Dictionary being added to</param>
+        /// <returns>The amount of words added</returns>
+        public static int ProcessFile(string fileName, int fileNums, Dictionary<string, WordCount> dic)
+        {
+            int count = 0;
+            using (StreamReader input = new StreamReader(fileName))
+            {
+                while (!input.EndOfStream)
+                {
+                    string pattern = "[^abcdefghijklmnopqrstuvwxyz]+";
+                    string[] words = Regex.Split(input.ReadLine().ToLower(), pattern);
+
+                    for(int i = 0; i < words.Length; i++)
+                    {
+                        if (words[i] != "")
+                        {
+                            WordCount wc;
+                            if (dic.TryGetValue(words[i], out wc))
+                            {
+                                wc.Increment(fileNums);
+                            }
+                            else
+                            {
+                                dic.Add(words[i], new WordCount(words[i], 2));
+                                dic.TryGetValue(words[i], out wc);
+                                wc.Increment(fileNums);
+                            }
+                            count++;
+                        }
+                    }
+                }
+            }
+            return count;
+        }
+
+        /// <summary>
+        /// Gets a set number of words with the highest frequencies
+        /// </summary>
+        /// <param name="dic">Dictionary of words</param>
+        /// <param name="wInFile">The array of file numbers</param>
+        /// <param name="wordsToGet">Amount of words to get</param>
+        /// <returns></returns>
+        public static MinPriorityQueue<float, WordFrequency> GetMostCommonWords(Dictionary<string, WordCount> dic, int[] wInFile, int wordsToGet)
+        {
+            MinPriorityQueue<float, WordFrequency> queue = new MinPriorityQueue<float, WordFrequency>();
+            foreach (KeyValuePair<string, WordCount> value in dic)
+            {
+                WordFrequency wf = new WordFrequency(value.Value, wInFile);
+                float comf = 0;
+                comf += wf[0];
+                comf += wf[1];
+                queue.Add(comf, wf);
+                if(queue.Count > wordsToGet)
+                {
+                    queue.RemoveMinimumPriority();
+                }
+            }
+            return queue;
+        }
+
+        /// <summary>
+        /// Gets the word frequency difference
+        /// </summary>
+        /// <param name="queue">The queue of word frequencies</param>
+        /// <returns>The diffence of word frequncies</returns>
+        public static float GetDifference(MinPriorityQueue<float, WordFrequency> queue)
+        {
+            double difSum = 0;
+            while (queue.Count > 0) {
+                WordFrequency wf = queue.RemoveMinimumPriority();
+                float freq1 = wf[0];
+                float freq2 = wf[1];
+                difSum += (freq1 - freq2) * (freq1 - freq2);
+            }
+            return (float) (100 *  Math.Sqrt(difSum));
+        }
+    } 
+}
---
+++ C# Projects/TextAnalyzer/Ksu.Cis300.TextAnalyzer/WordCount.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TextAnalyzer
+{
+    class WordCount
+    {
+        string _word;
+        int[] _count;
+
+        /// <summary>
+        /// Initializes words and count
+        /// </summary>
+        /// <param name="w">The string word</param>
+        /// <param name="c">The array of couns</param>
+        public WordCount(string w, int c)
+        {
+            _word = w;
+            _count = new int[c];
+        }
+
+        /// <summary>
+        /// Gets the _word variable
+        /// </summary>
+        public string Word
+        {
+            get 
+            {
+                return _word;
+            }
+
+        }
+
+        /// <summary>
+        /// Gets the _count variable
+        /// </summary>
+        public int[] NumberOfFiles
+        {
+            get
+            {
+                return _count;
+            }
+        }
+
+        /// <summary>
+        /// Returns a specific file number after given a file number
+        /// </summary>
+        /// <param name="k">File number given</param>
+        /// <returns>The file count at the file number</returns>
+        public int this[int n]
+        {
+            get
+            {
+                if(n < _count.Length - 1)
+                {
+                    return _count[n];
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Increments a given file number if it exists.
+        /// </summary>
+        /// <param name="fn">The file number given</param>
+        public void Increment(int fn)
+        {
+            if(fn < _count.Length)
+            {
+                _count[fn]++;
+            }
+            else
+            {
+                throw new ArgumentException();
+            }
+        }
+    }  
+}
