9f353f69d360f2ae4ac0025788a07b77b5986e61
rylankasitz <31218087+rylankasitz@users.noreply.github.com>
2018-02-05 21:38:35 -0600

    Added Text Analyzer

---
+++ C# Projects/homework-assignment-5-rylankasitz/Ksu.Cis300.TextAnalyzer/UserInterface.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Ksu.Cis300.Sort;
+
+namespace Ksu.Cis300.TextAnalyzer
+{
+    public partial class UserInterface : Form
+    {
+        public UserInterface()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Sets the file name of the first text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxSelectText1_Click(object sender, EventArgs e)
+        {
+            if (uxOpenDialog.ShowDialog() == DialogResult.OK)
+            {
+                uxText1.Text = uxOpenDialog.FileName.ToString();
+                if (uxText2.Text != "")
+                {
+                    uxAnalyze.Enabled = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets the file name of the second text
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxSelectText2_Click(object sender, EventArgs e)
+        {
+            if (uxOpenDialog.ShowDialog() == DialogResult.OK)
+            {
+                uxText2.Text = uxOpenDialog.FileName.ToString();
+                if(uxText1.Text != "")
+                {
+                    uxAnalyze.Enabled = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Shows the Difference value in a message box
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void uxAnalyze_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Dictionary<string, WordCount> dictionary = new Dictionary<string, WordCount>();
+                int[] wordCounts = new int[2];
+                wordCounts[0] = TestAnalyzer.ProcessFile(uxText1.Text, 0, dictionary);
+                wordCounts[1] = TestAnalyzer.ProcessFile(uxText2.Text, 1, dictionary);
+                MinPriorityQueue<float, WordFrequency> queue = TestAnalyzer.GetMostCommonWords(dictionary, wordCounts, Convert.ToInt32(uxNumber.Value));
+                MessageBox.Show("Difference measure: " + TestAnalyzer.GetDifference(queue).ToString());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
---
+++ C# Projects/homework-assignment-5-rylankasitz/Ksu.Cis300.TextAnalyzer/WordFrequency.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ksu.Cis300.TextAnalyzer
+{
+    class WordFrequency
+    {
+        private string _word;
+        private float[] _frequencies;
+
+        /// <summary>
+        /// initializes the word and frequencies
+        /// </summary>
+        /// <param name="o">Word object</param>
+        /// <param name="wc">Array of file counts</param>
+        public WordFrequency(WordCount o, int[] wc)
+        {
+            _word = o.Word;
+            _frequencies = new float[wc.Length];
+            if(o.NumberOfFiles.Length != wc.Length)
+            {
+                throw new ArgumentException();
+            }
+            for(int i = 0; i < _frequencies.Length; i++)
+            {
+                _frequencies[i] = (float) o.NumberOfFiles[i] / wc[i];
+            }
+        }
+
+        /// <summary>
+        /// Gets the private _word variable
+        /// </summary>
+        public string Word
+        {
+            get
+            {
+                return _word;
+            }
+        }
+
+        /// <summary>
+        /// Gets the frequecy of a give file number 
+        /// </summary>
+        /// <param name="n">File Number</param>
+        /// <returns>Frequency coresponding to a file number</returns>
+        public float this[int n]
+        {
+            get
+            {
+                if (n < _frequencies.Length)
+                {
+                    return _frequencies[n];
+                }
+                else
+                {
+                    throw new ArgumentException();
+                }
+            }
+        }
+    }
+}
